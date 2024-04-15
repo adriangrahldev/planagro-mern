b9c3407db2dda85836684354c7ddc4475271fdfa
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-15 12:00:13 -0400

    Añadir importación de UserProvider al Layout del Login y Register

--- client/src/app/user/layout.tsx
+++ client/src/app/user/layout.tsx
@@ -1,9 +1,14 @@
+"use client"
+import { UserProvider } from "@/contexts/UserContext";
 
 const SingleLayout = ({ children }: {children: React.ReactNode}) => {
     return (
+        <UserProvider>
+
         <div className="flex items-center justify-center w-full h-full">
             {children}
         </div>
+        </UserProvider>
     )
 }
 export default SingleLayout;
