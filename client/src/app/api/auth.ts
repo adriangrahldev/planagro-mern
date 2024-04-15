c429b5acd663c4e815515d2e972ad2f42f6c5601
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-15 08:59:45 -0400

    Añadir función de registro en auth.ts

--- client/src/app/api/auth.ts
+++ client/src/app/api/auth.ts
@@ -12,3 +12,16 @@ export const loginAction = (loginData: { email: string, password: string }) => {
 
   });
 }
+
+export const registerAction = (registerData: { fullName: string, email: string, password: string, confirmPassword: string }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/users/register`, registerData);
+      const data = await response.data;
+      resolve(data);
+    } catch (error) {
+      reject(error);
+    }
+
+  });
+}
