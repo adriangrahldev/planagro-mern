8f7f08229cbc8480bb00c370272405a2d53ead09
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-15 12:00:00 -0400

    Añadir UserProvider al Layout del Dashboard

--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -1,17 +1,19 @@
+"use client"
 import { Header } from "@/components/header/header.component";
 import { Sidebar } from "@/components/sidebar/sidebar.component";
+import { UserProvider } from "@/contexts/UserContext";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="dashboard-layout w-full h-full flex p-3">
-      <Sidebar />
-      <div className="dashboard-layout-content flex-1 flex flex-col">
-        <Header />
-        <div className="h-full p-4">
-            {children}
+    <UserProvider>
+      <div className="dashboard-layout w-full h-full flex p-3">
+        <Sidebar />
+        <div className="dashboard-layout-content flex-1 flex flex-col">
+          <Header />
+          <div className="h-full p-4">{children}</div>
         </div>
-    </div>
-    </div>
+      </div>
+    </UserProvider>
   );
 };
 export default DashboardLayout;
