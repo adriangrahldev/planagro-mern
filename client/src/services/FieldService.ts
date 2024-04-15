69b5f5fd57b1c6430087a1932a5c4a79185b51e1
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-15 18:31:35 -0400

    Actualizar servicio FieldService para incluir token de autenticación

--- client/src/services/FieldService.ts
+++ client/src/services/FieldService.ts
@@ -5,10 +5,17 @@ import axios from 'axios';
 
 
 const FieldService = {
-    getFields: async (): Promise<Field[]> => {
+    getFields: async (authToken: string): Promise<Field[]> => {
         return new Promise(async (resolve, reject) => {
             try {
-                const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/fields`);
+                const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/fields`,
+                    {
+                        withCredentials: true,
+                        headers: {
+                            Authorization: `Bearer ${authToken}`,
+                        },
+                    }
+                );
                 const data = await response.data;
                 resolve(data);
             } catch (error) {
