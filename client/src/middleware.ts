0e36919dff93e6eb1d4984c24c72e5ebafd78be6
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-15 08:59:21 -0400

    Corregir redireccionamiento en middleware.ts

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -3,11 +3,10 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  // Redirect to /home if the user is already logged in
-  return NextResponse.next();
+  return NextResponse.redirect(new URL('/user/login', 'http://localhost:3000').toString());
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/user/:path*',
+  matcher: '/',
 }
