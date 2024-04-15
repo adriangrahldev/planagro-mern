ae52b665da0c9daaf7b7b90d85c298cc83da05b9
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-15 18:31:48 -0400

    Actualizar página de campos en el dashboard

--- client/src/app/dashboard/fields/page.tsx
+++ client/src/app/dashboard/fields/page.tsx
@@ -1,28 +1,51 @@
+"use client"
 import { FieldsTable } from "@/components/fields/FieldsTable";
+import { useUser } from "@/contexts/UserContext";
 import { Field } from "@/interfaces/field.interface";
 import FieldService from "@/services/FieldService";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
-const FieldsPage = () => {
+ const FieldsPage = () => {
+
+  const {user} = useUser();
+
   const [fields, setFields] = useState<Field[]>([]);
 
+  
   useEffect(() => {
-    fetchFields();
-  }, []);
+    if(user){
+      fetchFields();
+    }
+
+  }, [user]);
 
   const fetchFields = async () => {
     try {
-      const fields = await FieldService.getFields();
+      
+      const fields = await FieldService.getFields(user?.authToken);
       setFields(fields);
     } catch (error) {
       console.error(error);
     }
   };
+  
 
   return (
-    <div>
-      <h1>Fields</h1>
-      <FieldsTable fields={fields} />
+    <div className="bg-green-100 p-4">
+      <div className="flex items-start justify-between">
+        <h1 className="text-xl font-semibold mb-4">Campos</h1>
+        <Link href="/dashboard/fields/create">
+          <PlusCircleIcon width={24}/>
+        </Link>
+      </div>
+
+      <div className="p-2 bg-white rounded-md">
+        <FieldsTable fields={fields} />
+      </div>      
+
     </div>
   );
 };
+export default FieldsPage;
