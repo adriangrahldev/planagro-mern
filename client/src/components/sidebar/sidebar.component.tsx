0bb0cdf0368d14ce0040be6e1a43f9140bb78a8d
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-16 12:05:07 -0400

    Actualizar estilos del Sidebar

--- client/src/components/sidebar/sidebar.component.tsx
+++ client/src/components/sidebar/sidebar.component.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+"use client"
 import { useUser } from "@/contexts/UserContext";
 import {
   HomeIcon,
@@ -9,7 +8,7 @@ import {
 } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const routes = [
@@ -17,22 +16,38 @@ const routes = [
     name: "Inicio",
     icon: HomeIcon,
     path: "/dashboard",
+    activators: [
+      "/dashboard",
+    ]
   },
   {
     name: "Campos",
     icon: MapIcon,
     path: "/dashboard/fields",
+    activators: [
+      "/dashboard/fields",
+      "/dashboard/fields/create",
+      "/dashboard/fields/:id",
+      "/dashboard/fields/:id/edit",
+    ]
   },
   {
     name: "Actividades",
     icon: ListBulletIcon,
-    path: "/dsahboard/activities",
+    path: "/dashboard/activities",
+    activators: [
+      "/dashboard/activities",
+      "/dashboard/activities/create",
+      "/dashboard/activities/:id",
+      "/dashboard/activities/:id/edit",
+    ]
   },
 ];
+
 export function Sidebar() {
-  
   const router = useRouter();
-  const [pathname, setPathname] = useState("/");
+  const currentPath = usePathname();
+
   const { logout } = useUser();
 
   const handleLogout = () => {
@@ -41,10 +56,11 @@ export function Sidebar() {
   };
 
   useEffect(() => {
-    setPathname(window.location.pathname);
-  }, []);
+    console.log(currentPath);
+  }, [currentPath]);
+
   return (
-    <div className="w-56 bg-[#e9e8e8] rounded-tl-md rounded-bl-md shadow-md flex flex-col">
+    <div className="w-56 bg-green-200 rounded-tl-md rounded-bl-md shadow-md flex flex-col">
       <div
         id="logo"
         className="flex items-center justify-center gap-2 text-xl px-2 pt-4 mb-10"
@@ -57,26 +73,25 @@ export function Sidebar() {
       <div className="flex-1 flex flex-col">
         <div>
           <ul className="flex flex-col gap-2 px-4">
-            {routes.map((route, index) => (
-              <li
-                key={index}
-                className={`p-2 rounded-md ${
-                  pathname === route.path
-                    ? "bg-[#cfc6c6] font-semibold shadow"
-                    : "hover:bg-[#ddd6d6]"
-                } `}
-              >
-                <Link href={route.path} className="flex gap-2">
-                  <route.icon width={24} />
-                  {route.name}
-                </Link>
-              </li>
-            ))}
+            {
+              routes.map((route, index) => (
+                <li key={index}>
+                  <Link href={route.path} className={`flex items-center gap-2 p-2 rounded-md hover:bg-green-300 ${
+                        route.activators.includes(currentPath) ? "bg-green-300" : ""
+                      }`}>
+                    
+                      <route.icon width={24} />
+                      <span>{route.name}</span>
+                    
+                  </Link>
+                </li>
+              ))
+            }
           </ul>
         </div>
         <div className="flex-1 flex items-end justify-center py-4">
           <button
-            className="rounded-md hover:bg-[#cfc6c6] p-2"
+            className="rounded-md hover:bg-white hover:shadow-lg p-2"
             onClick={() => handleLogout()}
           >
             <Link href="/logout" className="flex gap-2">
