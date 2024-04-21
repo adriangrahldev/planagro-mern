fd8326cdd0ce75c9c844598ee33010257af5e0c8
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-20 23:21:06 -0400

    Actualizar tipo de datos de coordenadas en field.model.ts

--- server/src/models/field.model.ts
+++ server/src/models/field.model.ts
@@ -3,17 +3,15 @@ import { Schema, model, Document } from 'mongoose';
 interface Field extends Document {
     name: string;
     surface: number;
-    latitude: number;
-    longitude: number;
+    coords: Schema.Types.Array;
     createdBy: Schema.Types.ObjectId | string; // Campo de referencia al usuario que creó el campo
 }
 
 const fieldSchema = new Schema<Field>({
     name: { type: String, required: true },
     surface: { type: Number, required: true },
-    latitude: { type: Number, required: true },
-    longitude: { type: Number, required: true },
+    coords: { type: Schema.Types.Array, required: true }, // Change the type to Schema.Types.Array
     createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true }
-});
+},{timestamps: true});
 
 export default model<Field>('Field', fieldSchema);
