93f724af49e734b9803bd052bf429ca82ed022e8
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-22 06:55:02 -0400

    Actualizar filtro de estado en page.tsx

--- client/src/app/dashboard/activities/page.tsx
+++ client/src/app/dashboard/activities/page.tsx
@@ -14,7 +14,7 @@ const ActivitiesPage = () => {
   const { user } = useUser();
 
   const [activities, setActivities] = useState<Activity[]>([]);
-
+  const [statusFilter, setStatusFilter] = useState<string>("all");
   useEffect(() => {
     if (user) {
       fetchActivities();
@@ -44,12 +44,17 @@ const ActivitiesPage = () => {
     try {
       // Check if the action result contains data and if it's an array
       if (activities && Array.isArray(activities)) {
-        const dataToExport = activities.map((activity: Activity) => ({
+        // filtrar las actividades por estado
+        let filteredActivities = activities;
+        if (statusFilter !== "all") {
+          filteredActivities = activities.filter((activity) => activity.status === statusFilter);
+        }
+        const dataToExport = filteredActivities.map((activity: Activity) => ({
           TITULO: activity.title,
           DESCRIPCION: activity.description,
           CAMPO: (activity.targetField as Field).name,
           FECHA: new Date(activity.date.split("T")[0]+" 12:00:00").toLocaleDateString(),
-          ESTADO: activity.status,
+          ESTADO: activity.status === "completed" ? "Completada" : "Pendiente",
         })
           ,);
         // Create Excel workbook and worksheet
@@ -70,7 +75,19 @@ const ActivitiesPage = () => {
   return (
     <div className="bg-green-100 p-4">
       <div className="flex items-start justify-between">
+      <div className="flex gap-12">
         <h1 className="text-xl font-semibold mb-4">Actividades</h1>
+        <select name="" id=""
+        className=" rounded-md min-w-44 bg-white h-7 border-gray-300 shadow-md"
+        value={statusFilter}
+        onChange={(e) => setStatusFilter(e.target.value)}
+        >
+          <option value="all">Todas</option>
+          <option value="completed">Completadas</option>
+          <option value="pending">Pendientes</option>
+        </select>
+      </div>
+
         <div className="flex gap-2">
           <Link
             href="/dashboard/activities/create"
@@ -88,7 +105,7 @@ const ActivitiesPage = () => {
       </div>
 
       <div className="p-2 bg-white rounded-md">
-        <ActivitiesTable activities={activities} handleDelete={handleDelete} />
+        <ActivitiesTable statusFilter={statusFilter} activities={activities} handleDelete={handleDelete} />
       </div>
     </div>
   );
--- client/src/components/activities/ActivitiesTable.tsx
+++ client/src/components/activities/ActivitiesTable.tsx
@@ -1,66 +1,140 @@
 import { Activity } from "@/interfaces/activity.interface";
 import { Field } from "@/interfaces/field.interface";
-import { ExclamationCircleIcon, EyeIcon, PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
+import {
+  ExclamationCircleIcon,
+  EyeIcon,
+  PencilIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
 
-export const ActivitiesTable = ({activities, handleDelete}:{activities:Activity[], handleDelete: CallableFunction}) => {
+export const ActivitiesTable = ({
+  activities,
+  handleDelete,
+  statusFilter,
+}: {
+  activities: Activity[];
+  handleDelete: CallableFunction;
+  statusFilter: string;
+}) => {
+  return (
+    <table className="w-full text-left">
+      <thead className="text-gray-500">
+        <tr>
+          <th className="p-2">Título</th>
+          <th className="p-2">Descripción</th>
+          <th className="p-2">Fecha</th>
+          <th className="p-2">Estado</th>
+          <th className="p-2">Campo</th>
+          <th className="p-2">Accciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {statusFilter === "pending" &&
+          activities.filter((activity) => activity.status === "pending")
+            .length === 0 && (
+            <tr>
+              <td colSpan={5} className="text-center border-t text-red-800">
+                <div className="py-2">
+                  <ExclamationCircleIcon
+                    className="inline"
+                    width={30}
+                  ></ExclamationCircleIcon>{" "}
+                  No hay actividades pendientes
+                </div>
+              </td>
+            </tr>
+          )}
 
+        {statusFilter === "completed" &&
+          activities.filter((activity) => activity.status === "completed")
+            .length === 0 && (
+            <tr>
+              <td colSpan={5} className="text-center border-t text-red-800">
+                <div className="py-2">
+                  <ExclamationCircleIcon
+                    className="inline"
+                    width={30}
+                  ></ExclamationCircleIcon>{" "}
+                  No hay actividades completadas
+                </div>
+              </td>
+            </tr>
+          )}
 
+          {
+            statusFilter === "all" &&
+            activities.length === 0 && (
+              <tr>
+                <td colSpan={5} className="text-center border-t text-red-800">
+                  <div className="py-2">
+                    <ExclamationCircleIcon
+                      className="inline"
+                      width={30}
+                    ></ExclamationCircleIcon>{" "}
+                    No hay actividades registradas
+                  </div>
+                </td>
+              </tr>
+            )
+          }
 
-    return (
-        <table className="w-full text-left">
-            <thead className="text-gray-500">
-                <tr >
-                    <th className="p-2">Título</th>
-                    <th className="p-2">Descripción</th>
-                    <th className="p-2">Fecha</th>
-                    <th className="p-2">Estado</th>
-                    <th className="p-2">Campo</th>
-                    <th className="p-2">Accciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                {activities.map((activity, index) => (
-                    <tr key={index}>
-                        <td className="p-2">{activity.title}</td>
-                        <td className="p-2">{activity.description}</td>
-                        <td className="p-2">
-                            {new Date(activity.date.split("T")[0]+" 12:00:00").toLocaleDateString()}
-                        </td>
-                        <td className="p-2">
-                            <span className={`px-2 py-1 ${activity.status === 'pending' ? 'bg-yellow-200 text-gray-600 ' : 'bg-green-400 text-white'} text-sm font-semibold rounded-md shadow`}>
-                                {activity.status === 'pending' ? 'PENDIENTE' : 'COMPLETADO'}
-                            </span>                        
-                        </td>
-                        <td className="p-2">{(activity.targetField as Field)?.name}</td>
-                        <td className="p-2">
+            {
+                activities.map((activity, index) => {
+                    if (activity.status === statusFilter.toString() || statusFilter === "all") {
+                    return (
+
+                        <tr key={index}>
+                            <td className="p-2">{activity.title}</td>
+                            <td className="p-2">{activity.description}</td>
+                            <td className="p-2">
+                            {new Date(
+                                activity.date.split("T")[0] + " 12:00:00"
+                            ).toLocaleDateString()}
+                            </td>
+                            <td className="p-2">
+                            <span
+                                className={`px-2 py-1 ${
+                                activity.status === "pending"
+                                    ? "bg-yellow-200 text-gray-600 "
+                                    : "bg-green-400 text-white"
+                                } text-sm font-semibold rounded-md shadow`}
+                            >
+                                {activity.status === "pending" ? "PENDIENTE" : "COMPLETADO"}
+                            </span>
+                            </td>
+                            <td className="p-2">{(activity.targetField as Field)?.name}</td>
+                            <td className="p-2">
                             <div className="flex gap-2">
-
-                                <Link href={`/dashboard/activities/${activity._id}/show`} className="bg-green-400 p-2 rounded-full">
-                                    <EyeIcon className="h-4 w-4"/>
+                                <Link
+                                href={`/dashboard/activities/${activity._id}/show`}
+                                className="bg-green-400 p-2 rounded-full"
+                                >
+                                <EyeIcon className="h-4 w-4" />
                                 </Link>
-                                <Link  href={`/dashboard/activities/${activity._id}/edit`} className="bg-green-200 p-2 rounded-full">
-                                    <PencilIcon className="h-4 w-4"/>
+                                <Link
+                                href={`/dashboard/activities/${activity._id}/edit`}
+                                className="bg-green-200 p-2 rounded-full"
+                                >
+                                <PencilIcon className="h-4 w-4" />
                                 </Link>
-                                <button onClick={(e) => {handleDelete(activity._id)}} className="p-2 border-green-200 border rounded-full">
-                                    <TrashIcon className="h-4 w-4"/>
+                                <button
+                                onClick={(e) => {
+                                    handleDelete(activity._id);
+                                }}
+                                className="p-2 border-green-200 border rounded-full"
+                                >
+                                <TrashIcon className="h-4 w-4" />
                                 </button>
                             </div>
-                        </td>
-                    </tr>
-                ))}
-                {
-                    activities.length === 0 && (
-                        <tr>
-                            <td colSpan={5} className="text-center border-t text-red-800"> 
-                                <div className="py-2">
-                                    <ExclamationCircleIcon className="inline" width={30}></ExclamationCircleIcon> No hay actividades registradas
-                                </div>
                             </td>
                         </tr>
                     )
                 }
-            </tbody>
-        </table>
-    )
-}
+                })
+            }
+        
+      </tbody>
+    </table>
+  );
+};
