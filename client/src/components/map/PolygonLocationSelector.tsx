ad30665ef6078440dd74f69e0bcbea487d26bd63
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-22 04:26:02 -0400

    Actualizar manejo de coordenadas en PolyLocationSelector.tsx

--- client/src/components/map/PolygonLocationSelector.tsx
+++ client/src/components/map/PolygonLocationSelector.tsx
@@ -1,126 +1,116 @@
-"use client"
+"use client";
 import React, { use, useEffect, useState } from "react";
-import { GoogleMap, useLoadScript, Marker, Polygon } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  Polygon,
+  PolygonF,
+  useJsApiLoader,
+} from "@react-google-maps/api";
+import { LocationService } from "@/services/LocationService";
 
-const libraries = ["places"];
 
 const PolyLocationSelector = ({
-  coords = [],
-  setCoords = () => {},
+  initialCoords = [],
+  setNewCoords = () => {},
   readOnly = false,
 }: {
-  coords?: { lat: number; lng: number }[];
-  setCoords?: (coords: { lat: number; lng: number }[]) => void;
+  initialCoords?: { lat: number; lng: number }[];
+  setNewCoords?: (coords: { lat: number; lng: number }[]) => void;
   readOnly?: boolean;
 }) => {
-  const [markers, setMarkers] = useState(coords);
+  const libraries = ["places"];
+  const [coords, setCoords] = useState<null | any[]>(null);
   const [center, setCenter] = useState({ lat: 0, lng: 0 });
-
+  
   const mapOptions: google.maps.MapOptions = {
-    disableDefaultUI: true,
-    clickableIcons: true,
+    clickableIcons: false,
     scrollwheel: true,
     draggable: !readOnly,
     zoomControl: true,
     streetViewControl: false,
     fullscreenControl: false,
-    mapTypeControl: true,
+    mapTypeControl: false,
+    noClear: false,
   };
 
-  const { isLoaded } = useLoadScript({
+  const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: "AIzaSyAwHhg92jVfj3JYfvvw7Qr5fNKXdMFfl4I",
     libraries: libraries as any,
+    id: "google-map-script",
   });
 
   useEffect(() => {
-    if (coords.length > 0) {
-      const newMarkers = coords.map((coord) => ({
-        lat: coord.lat,
-        lng: coord.lng,
-      }));
-      setMarkers(newMarkers);
-      setCenter(newMarkers[0]);
+    if (initialCoords!.length > 0) {
+      setCenter(initialCoords![0]);
+      setCoords(initialCoords);
     } else {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
+      LocationService.getNavigatorLocation()
+        .then((position: any) => {
           setCenter({
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           });
+        })
+        .catch((error: any) => {
+          alert("No se pudo obtener la ubicación actual");
+          console.error(error);
         });
-      } else {
-        console.log("Geolocation is not supported by this browser.");
-      }
     }
   }, []);
 
-
   useEffect(() => {
-    if (!readOnly) {
-      setCoords(markers);
+    if (coords) {
+      setNewCoords(coords);
     }
-  }, [markers]);
-
-  const onMarkerDrag = (e: any, marker: any) => {
-    if (!readOnly) {
-      const newMarker = {
-        lat: e.latLng.lat(),
-        lng: e.latLng.lng(),
-      };
-      const updatedMarkers = markers.map((m) => (m === marker ? newMarker : m));
-      setMarkers(updatedMarkers);
-    }
-  };
+  }, [coords]);
 
   if (!isLoaded) return <p>Loading...</p>;
-
-  return (
-    <div>
-      <GoogleMap
-        options={mapOptions}
-        zoom={14}
-        center={center}
-        mapTypeId={google.maps.MapTypeId.ROADMAP}
-        mapContainerStyle={{ width: "800px", height: "300px" }}
-        onClick={(e) => {
-          if (!readOnly) {
-            const newMarker = {
-              lat: e.latLng?.lat() || 0,
-              lng: e.latLng?.lng() || 0,
-            };
-            setMarkers([...markers, newMarker]);
-          }
-        }}
-      >
-        <Polygon
-          paths={markers}
-          options={{
-            fillColor: "#f00",
-            fillOpacity: 0.35,
-            strokeColor: "#f00",
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
+  else
+    return (
+      <div>
+        <GoogleMap
+          options={mapOptions}
+          zoom={16}
+          center={center}
+          mapTypeId={google.maps.MapTypeId.HYBRID}
+          mapContainerStyle={{ width: "800px", height: "300px" }}
+          onClick={(e) => {
+            if (!readOnly) {
+              const newMarker = {
+                lat: e.latLng?.lat() || 0,
+                lng: e.latLng?.lng() || 0,
+              };
+              setCoords(coords ? [...coords, newMarker] : [newMarker]);
+            }
           }}
-        />
-
-        {markers.map((marker, index) => (
-          <Marker
-            draggable={!readOnly}
-            onDragEnd={(e) => onMarkerDrag(e, marker)}
-            key={index}
-            position={marker}
-            icon={{
-              path: google.maps.SymbolPath.CIRCLE,
-              scale: 5,
-              fillColor: "#f00",
-              fillOpacity: 1,
-              strokeWeight: 0,
-            }}
-          />
-        ))}
-      </GoogleMap>
-    </div>
-  );
+        >
+          {coords?.length! > 0 && (
+            <PolygonF
+              path={coords as any[]}
+              options={{
+                fillColor: "#f00",
+                fillOpacity: 0.35,
+                strokeColor: "#f00",
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                editable: !readOnly,
+                strokePosition: google.maps.StrokePosition.CENTER,
+              }}
+              editable={!readOnly}
+              onEdit={(e) => {
+                const newCoords = e
+                  .getPath()
+                  .getArray()
+                  .map((coord) => {
+                    return { lat: coord.lat(), lng: coord.lng() };
+                  });
+                setCoords(newCoords);
+              }}
+            />
+          )}
+        </GoogleMap>
+      </div>
+    );
 };
 
 export default PolyLocationSelector;
