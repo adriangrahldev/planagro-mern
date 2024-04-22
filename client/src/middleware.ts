e27b9769b7b2b7666e38e0a1f3ef1f7ccc93814b
Adrian Grahl <adriangrahlmaciel@gmail.com>
2024-04-22 05:50:32 -0400

    Actualizar URL de redirección en middleware.ts

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 export async function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/user/login', 'http://localhost:3000').toString());
+  return NextResponse.redirect(new URL('/user/login', 'https://planagro.adriangrahl.dev').toString());
 }
  
 export const config = {
